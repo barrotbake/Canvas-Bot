11bac62fa8648e4ecbd19cf5746e8823ffaccc62
ryan <ryanbroguiere123@gmail.com>
2021-09-26 15:40:25 -0700

    placeholder landing page

--- web-portal/src/App.js
+++ web-portal/src/App.js
@@ -2,8 +2,50 @@ import './App.css';
 
 let App = () => {
   return (
+<<<<<<< Updated upstream
     <div className="App">
+=======
+    <div class = "Desktop">
+      <header class = "Frame2">
+        <div class = "NavGroup1">
+          <img class = "Ellipse1" src = "https://i.pinimg.com/originals/ae/07/d0/ae07d091b98545facd87c7c597569670.jpg" alt="logo"></img>
+          <div class = "SiteTitle">Canvas Bot</div>
+        </div>
+        <div class = "Frame1">
+          <text class = "Login">Login</text>
+          <text class = "Page1">Page 1</text>
+          <text class = "Page2">Page 2</text>
+        </div>
+
+      </header>
+
+      <img class = "Ellipse2" src = "https://i.pinimg.com/originals/ae/07/d0/ae07d091b98545facd87c7c597569670.jpg" alt="logo">
+      </img>
+
+      <div class = "Description">
+        Witty and Eye Catching Header Here
+      </div>
+      <div class = "Description2">
+        Lorem ipsum dolor sit amet
+      </div>
+      
+      <div class = "Frame6">
+        <div class = "Group3">
+          <div class = "Group5">
+            <text class = "JoinDiscord rect1">Join Discord</text>
+          </div>
+        </div>
+        <div class = "Group2">
+          <div class = "Group4">
+            <text class = "InviteBot rect2">Invite Bot</text>
+          </div>
+        </div>
+      </div>
+      
+      
+>>>>>>> Stashed changes
     </div>
+    
   );
 }
 
