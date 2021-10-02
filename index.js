73ec5576816ea87e41b44ab6cd424cfd663b8197
Ryan williams <70562805+ryanwillyams@users.noreply.github.com>
2021-10-01 19:13:13 -0700

    Add files via upload
    Can send information to discord VIA endpoints.

--- index.js
+++ index.js
@@ -2,19 +2,21 @@
 This file will get the announcement message off of our 362 Class and package.json will show the dependencies. 
 You will need to use your own access token in the "authorization" property
 UPDATE V2: Added function that updates on an interval of 1 minute. Once we have a DB up we can get custom user course ID.
+UPDATE V3: Can now communicate with the discord server itself and pull information from the canvas as well
 */
-import express, { json } from 'express';
+import express, { json, response } from 'express';
 import fetch from 'node-fetch';
-import http from 'http';
+var app = express();
+import  http from 'http';
 import { stringify } from 'querystring';
-import { getRecord } from './dbUtil.js';
 const { Headers } = fetch;
 
-var app = express();
 const PORT = 8080;
 
 
 const url = "https://csufullerton.instructure.com/api/v1/";
+
+const user = {content: 'hello world'}
 
 
 
@@ -26,13 +28,14 @@ var data = '2021-08-19';
 
 //setInterval(intervalFunc2,60000);
 
+
 function intervalFunc() {
      
    // setInterval(intervalFunc,60000);
 fetch(url + `/announcements?context_codes[]=${obj}&start_date=${data}` , {
     method: "GET",
     headers : {
-        'Authorization' : 'Bearer <Your access token>',
+        'Authorization' : 'Bearer <Auth Token>',
         'Content-Type' : 'application/json'
     }
     
@@ -46,27 +49,47 @@ fetch(url + `/announcements?context_codes[]=${obj}&start_date=${data}` , {
 .catch(err => console.log(err))
 }
 
-function intervalFunc2() {
+//function intervalFunc2() {
      
-  
-fetch(url + `courses/${course}/discussion_topics` , {
-    method: "GET",
-    headers : {
-        'Authorization' : 'Bearer <YOUR ACCESS TOKEN>',
-        'Content-Type' : 'application/json'
-    }
-    
-})
-.then(res => res.json())
-.then(data => { 
-       
-    console.log(data[0].id);
-    console.log(data[0].title);
-    console.log(data[0].message);
-}
- ) 
-.catch(err => console.log(err))
-}
+app.all('/whatever', async(response) =>{
+    (async function main() {
+        try {
+          const res1 = await fetch(url + `courses/${course}/discussion_topics`, {
+            method: "GET",
+            headers: {
+              Authorization: "Bearer <Auth Token>",
+              "Content-Type": "application/json",
+            },
+          });
+          const data1 = await res1.json();
+          console.log(data1);
+          const string = [data1[0].title, data1[0].message];
+          const res2 = await fetch(
+            "https://discordapp.com/api/channels/890800027985903686/messages",
+            {
+              method: "POST",
+              headers: {
+                "Authorization": "Bot <Auth Token>", 
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                
+                content: string.join('\n').replace('<p>', '').replace('</p>','')
+              }),
+            }
+          );
+      
+          const data2 = await res2.json();
+          console.log(data2);
+        } catch (err) {
+          console.log(err);
+        }
+      })();
+
+});
+
+
 
 
 app.listen(PORT);
