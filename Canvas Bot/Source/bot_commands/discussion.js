cfad5c0e428be8bf2a810c49eed7eebd3ca603dc
AlanBlandon <alanblandon@csu.fullerton.edu>
2021-10-02 23:19:09 -0700

    Updated discussion.js

--- Canvas Bot/Source/bot_commands/discussion.js
+++ Canvas Bot/Source/bot_commands/discussion.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'discussion',
     execute(message, args, Discord){
         const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-        fetch(`http://76.174.211.132:8080/whatever`)
+        fetch(`http://IP_ADDRESS:ds/endpoint`)
     }
 }
