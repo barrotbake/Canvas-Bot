8d62ad7d921e1359761c5caebb1c8f1ae555f0db
Ryan Williams <ryanwillyamsdo@gmail.com>
2021-10-19 15:05:11 -0700

    Added a bunch of changes to make it less hardcoded

--- index.js
+++ index.js
@@ -5,29 +5,26 @@ UPDATE V2: Added function that updates on an interval of 1 minute. Once we have 
 UPDATE V3: Can now communicate with the discord server itself and pull information from the canvas as well
 UPDATE V4: Cleaned up index.js for only thing that may be necessary. Added a fetch folder.
 */
-import express, { json, query, response } from 'express';
+import express, { json, query, request, response } from 'express';
 import { assignments , announcements , discussions} from "./Fetch/fetchCalls.js";
 var app = express();
 app.use(express.json());
 const PORT = 8080;
+app.listen(PORT);
 
-
-
+var guild;
+var channel;
 
 //setInterval(intervalFunc2,60000);
 
 
 //function intervalFunc2() {
      
- app.all('/assignments', assignments()  
- );
-app.all('/discussions', discussions());
-app.all('/announcements', announcements());
+ app.all('/assignments',req => assignments(guild = req.body.guild,channel =req.body.channel));
+app.all('/discussions',req => discussions(guild = req.body.guild,channel =req.body.channel));
+app.all('/announcements', req => announcements(guild = req.body.guild,channel =req.body.channel));
 
 
 
 
 
-
-
-app.listen(PORT);
