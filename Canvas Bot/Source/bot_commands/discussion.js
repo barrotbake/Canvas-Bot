02e6301a7432657a1903f37c699c6ec82a42b0d3
AlanBlandon <alanblandon@csu.fullerton.edu>
2021-10-28 17:38:18 -0700

    Updated discussions.js

--- Canvas Bot/Source/bot_commands/discussion.js
+++ Canvas Bot/Source/bot_commands/discussion.js
@@ -1,7 +1,26 @@
 module.exports = {
-    name: 'discussion',
+    name: 'discussions',
     execute(message, args, Discord){
+        const update = require('../main.js');
+        const config = require('../Data/config.json');
+        const {MongoClient} = require('mongodb');
+
+        const uri = "mongodb+srv://Alan_B:AlanBlandon@cluster0.ab9yi.mongodb.net/myFirstDatabase?authSource=admin&replicaSet=atlas-836il4-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
+        const mongo_client = new MongoClient(uri);
+        
         const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-        fetch(`http://IP_ADDRESS:ds/endpoint`)
+        let server = message.guild.id;
+        let channel = message.channel.id;
+        let discord_id = {"guild": `${server}`, "channel": `${channel}`}
+
+        update.updateListing(mongo_client, String(server), { guild_id: server });
+        fetch(`http://${config.IP}:${config.Port}/discussions`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:   JSON.stringify(discord_id)
+        })
+        console.log(discord_id)
     }
 }
