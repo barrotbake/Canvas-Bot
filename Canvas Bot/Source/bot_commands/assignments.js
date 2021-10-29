a18a3dd46ab59b7bec3eecfc0faca1ef4def48fb
AlanBlandon <alanblandon@csu.fullerton.edu>
2021-10-28 17:50:59 -0700

    Added assignments.js

---
+++ Canvas Bot/Source/bot_commands/assignments.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: 'assignments',
+    execute(message, args, Discord){
+        const update = require('../main.js');
+        const config = require('../Data/config.json');
+        const {MongoClient} = require('mongodb');
+
+        const uri = "uri"
+        const mongo_client = new MongoClient(uri);
+        
+        const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+        let server = message.guild.id;
+        let channel = message.channel.id;
+        let discord_id = {"guild": `${server}`, "channel": `${channel}`}
+
+        update.updateListing(mongo_client, String(server), { guild_id: server });
+        fetch(`http://${config.IP}:${config.Port}/assignments`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:   JSON.stringify(discord_id)
+        })
+        console.log(discord_id)
+    }
+}
