5604ca884c556a1fb248b9aeff4b4cfd424bb452
AlanBlandon <alanblandon@csu.fullerton.edu>
2021-10-28 17:31:49 -0700

    Updated studysession.js

--- Canvas Bot/Source/bot_commands/studysession.js
+++ Canvas Bot/Source/bot_commands/studysession.js
@@ -2,15 +2,18 @@ module.exports = {
     name: 'studysession',
     description: 'This command creates both text and voice channels under a newly created category.',
     execute(message, args, Discord){
+
         const category = message.guild.channels.cache.find(c => c.name == 'STUDY SESSION' && c.type == 'GUILD_CATEGORY');
         if(category){
             message.reply("A study session has already started.");
         }
         else{
+        /*******************Category/Channel-Creation*******************/
         var textChannelArray = ['notes-resources', 'homework-help']
         var voiceChannelArray = ['study-room-1', 'study-room-2', 'study-room-3']
         message.guild.channels.create('STUDY SESSION', { type: 'GUILD_CATEGORY' });
 
+        /*********************Text-Channel-Creation*********************/
         message.guild.channels.create(textChannelArray[0])
         .then(channel => {
         let category = message.guild.channels.cache.find(c => c.name == 'STUDY SESSION' && c.type == 'GUILD_CATEGORY');
@@ -25,6 +28,7 @@ module.exports = {
         channel.setParent(category.id);
         }).catch(console.error);
 
+        /*********************Voice-Channel-Creation*********************/
         message.guild.channels.create(voiceChannelArray[0], {
             type: 'GUILD_VOICE'
         })
