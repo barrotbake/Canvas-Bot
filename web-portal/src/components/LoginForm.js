e8da133909cc1191e5253e635b26396af8876e9d
ryan <ryanbroguiere123@gmail.com>
2021-11-09 21:23:14 -0800

    login sends data to axios

--- web-portal/src/components/LoginForm.js
+++ web-portal/src/components/LoginForm.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from "react-router-dom";
+import axios from 'axios';
+
+
 
 const LoginForm = () => {
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password:"",
+    
+  });
+
+let handleUsernameChange = (event) => {
+    let newData = {username: event.target.value , password: formData.password}
+    setFormData(newData)
+    console.log(formData)
+    
+  }
+
+  let handlePasswordChange = (event) => {
+    let newData = {username: formData.username , password: event.target.value}
+    setFormData(newData)
+    console.log(formData)
+    
+  }
+
+  let onSubmit = e => {
+    e.preventDefault();
+  
+     // When post request is sent to the create url, axios will add a new doc to the database.
+     const newperson = {
+         email: formData.username,
+         password: formData.password,
+         canvasId: "7394573897"
+     };
+  
+     axios
+       .post("http://localhost:5000/login/add", newperson)
+       .then((res) => console.log(res.data));
+ }
+
     return (
 <div class="h-auto pb-24 bg-right bg-cover"> 
 <div class = "flex flex-col items-center justify-center slide-in-bottom mt-20" id="showLogin">
@@ -9,10 +48,10 @@ const LoginForm = () => {
   <form class="flex flex-col bg-white border-2 rounded-lg shadow-lg p-12 mt-5">
   <h1 class="mb-5 text-xl font-light text-left sm:text-center">Log in to your account</h1>
   <label class="font-semibold text-xs" htmlFor="username">Username</label>
-  <input class="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text" name="username" id="username" minLength="4" maxLength="16" required/>
+  <input class="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text" name="username" id="username" minLength="4" maxLength="16" required onChange={handleUsernameChange}/>
   <label class="font-semibold text-xs mt-3" htmlFor="password">Password</label>
-  <input class="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="password" name="password" id="password" minLength="8" maxLength="20" required/>
-  <button id="loginButton" class="flex items-center justify-center h-12 px-6 w-64 bg-blue-400 mt-8 rounded-xl font-semibold text-md text-white hover:bg-blue-700 hover:shadow-xl transition duration-700 ease-in-out transform hover:-translate-y-2 hover:scale-105">Login</button>
+  <input class="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="password" name="password" id="password" minLength="8" maxLength="20" required onChange={handlePasswordChange}/>
+  <button id="loginButton" class="flex items-center justify-center h-12 px-6 w-64 bg-blue-400 mt-8 rounded-xl font-semibold text-md text-white hover:bg-blue-700 hover:shadow-xl transition duration-700 ease-in-out transform hover:-translate-y-2 hover:scale-105" onClick={onSubmit}>Login</button>
    <div class="flex mt-6 justify-center text-xs">
     Don't have an account?
     <Link to ='/signup' className="text-blue-400 hover:text-blue-600 hover:underline ml-1 loginButton"> Sign Up </Link>
